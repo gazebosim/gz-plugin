6266687916165dce12d32e5bc4febd0712e6bb3b
Michael Carroll <michael@openrobotics.org>
2020-06-22 21:30:17 -0500

    Fix loader and integration tests
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- loader/src/Loader_TEST.cc
+++ loader/src/Loader_TEST.cc
@@ -18,11 +18,52 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <fstream>
+
+#include <ignition/common/Filesystem.hh>
+#include <ignition/common/Util.hh>
 
 #include <ignition/plugin/Loader.hh>
 #include <ignition/plugin/config.hh>
 
 #include <ignition/plugin/SpecializedPluginPtr.hh>
+
+
+/////////////////////////////////////////////////
+bool IgnPluginLib(std::string &_path)
+{
+#ifdef IGN_PLUGIN_LIB
+  _path = IGN_PLUGIN_LIB;
+  return true;
+#else
+  std::string dataDir;
+  if (ignition::common::env("TEST_SRCDIR", dataDir))
+  {
+    _path = ignition::common::joinPaths(dataDir,
+        "__main__/ign_plugin/core/libignition-plugin2.so");
+    return true;
+  }
+#endif
+  return false;
+}
+
+/////////////////////////////////////////////////
+bool IgnDummyPluginLib(std::string &_path)
+{
+#ifdef IGNDummyPlugins_LIB
+  _path = IGNDummyPlugins_LIB;
+  return true;
+#else
+  std::string dataDir;
+  if (ignition::common::env("TEST_SRCDIR", dataDir))
+  {
+    _path = ignition::common::joinPaths(dataDir,
+        "__main__/ign_plugin/test/IGNDummyPlugins.so");
+    return true;
+  }
+#endif
+  return false;
+}
 
 /////////////////////////////////////////////////
 TEST(Loader, InitialNoInterfacesImplemented)
@@ -43,15 +84,22 @@ TEST(Loader, LoadNonexistantLibrary)
 TEST(Loader, LoadNonLibrary)
 {
   ignition::plugin::Loader loader;
-  EXPECT_TRUE(loader.LoadLib(std::string(IGN_PLUGIN_SOURCE_DIR)
-                             + "/core/src/Plugin.cc").empty());
+
+  std::ofstream tmpOut("test.tmp");
+  tmpOut.close();
+
+  EXPECT_TRUE(loader.LoadLib("test.tmp").empty());
 }
 
 /////////////////////////////////////////////////
 TEST(Loader, LoadNonPluginLibrary)
 {
   ignition::plugin::Loader loader;
-  EXPECT_TRUE(loader.LoadLib(IGN_PLUGIN_LIB).empty());
+
+  std::string libPath;
+  ASSERT_TRUE(IgnPluginLib(libPath));
+
+  EXPECT_TRUE(loader.LoadLib(libPath).empty());
 }
 
 /////////////////////////////////////////////////
@@ -84,11 +132,14 @@ TEST(Loader, DoubleLoad)
   // when a user asks for a library to be loaded twice.
   ignition::plugin::Loader loader;
 
-  loader.LoadLib(IGNDummyPlugins_LIB);
+  std::string libPath;
+  ASSERT_TRUE(IgnDummyPluginLib(libPath));
+
+  loader.LoadLib(libPath);
   const std::size_t interfaceCount = loader.InterfacesImplemented().size();
   EXPECT_LT(0u, interfaceCount);
 
-  loader.LoadLib(IGNDummyPlugins_LIB);
+  loader.LoadLib(libPath);
   EXPECT_EQ(interfaceCount, loader.InterfacesImplemented().size());
 }
 
@@ -96,20 +147,22 @@ TEST(Loader, DoubleLoad)
 TEST(Loader, ForgetUnloadedLibrary)
 {
   // These tests are for line coverage.
+  std::string libPath;
+  ASSERT_TRUE(IgnDummyPluginLib(libPath));
 
   // This first test triggers lines for the case that:
   //   1. A library is not loaded, and
   //   2. We tell a loader to forget the library
   ignition::plugin::Loader loader;
-  EXPECT_FALSE(loader.ForgetLibrary(IGNDummyPlugins_LIB));
+  EXPECT_FALSE(loader.ForgetLibrary(libPath));
 
   // This next test triggers lines for the case that:
   //   1. A library is loaded by some loader in the application, and
   //   2. We tell a *different* loader to forget the library
   ignition::plugin::Loader hasTheLibrary;
-  EXPECT_LT(0u, hasTheLibrary.LoadLib(IGNDummyPlugins_LIB).size());
+  EXPECT_LT(0u, hasTheLibrary.LoadLib(libPath).size());
 
-  EXPECT_FALSE(loader.ForgetLibrary(IGNDummyPlugins_LIB));
+  EXPECT_FALSE(loader.ForgetLibrary(libPath));
 }
 
 /////////////////////////////////////////////////
