e4f5242154bec52686f1a1d0da6b7f4808052e8c
Steve Peters <scpeters@openrobotics.org>
2022-05-23 06:43:35 -0700

    config.hh: using namespace gz with pragma message
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- core/include/ignition/plugin/config.hh
+++ core/include/ignition/plugin/config.hh
@@ -15,4 +15,21 @@
  *
  */
 
+#ifndef IGNITION_PLUGIN__CONFIG_HH_
+#define IGNITION_PLUGIN__CONFIG_HH_
+
 #include <gz/plugin/config.hh>
+
+namespace gz
+{
+}
+
+namespace ignition
+{
+  #ifndef SUPPRESS_IGNITION_HEADER_DEPRECATION
+    #pragma message("ignition namespace is deprecated! Use gz instead!")
+  #endif
+  using namespace gz;
+}
+
+#endif
